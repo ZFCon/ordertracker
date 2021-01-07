760afafac1d93e258ae5d890054f051e44f61035
unknown <ziad32ahmed111@gmail.com>
2021-01-07 21:06:40 +0200

    change the right translation

--- order/models.py
+++ order/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 
@@ -7,3 +7,7 @@ class Order(models.Model):
     request = models.TextField(help_text=_('Say what you want.'))
     doer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
                              null=True, blank=True, help_text=_('This should have the user that agreed to do it.'))
+
+    class Meta:
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
