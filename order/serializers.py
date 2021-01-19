da9a345c0ed35eb0c75b1db47bfab4addd6e6400
unknown <ziad32ahmed111@gmail.com>
2021-01-20 01:07:40 +0200

    update order serializers

--- order/serializers.py
+++ order/serializers.py
@@ -3,8 +3,18 @@ from rest_framework import serializers
 from .models import *
 
 
+class DoerRequestSerializer(serializers.ModelSerializer):
+    doer = serializers.StringRelatedField()
+
+    class Meta:
+        model = DoerRequest
+        fields = '__all__'
+
+
 class OrderSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField()
+    doer = serializers.StringRelatedField()
+    requests = DoerRequestSerializer(many=True)
 
     class Meta:
         model = Order
