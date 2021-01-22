55eafd5b431d28feaf667e82c4c346721acbf9da
unknown <ziad32ahmed111@gmail.com>
2021-01-23 01:53:54 +0200

    change serializer to the new structure

--- order/serializers.py
+++ order/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models.query import QuerySet
 from rest_framework import serializers
 
 from .models import *
@@ -6,9 +5,15 @@ from .serializer_defaults import OrderUrlDefault
 from .validators import HasDoderValidator
 from user.serializers import UserSerializer
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class DoerRequestSerializer(serializers.ModelSerializer):
-    doer = UserSerializer(default=serializers.CurrentUserDefault())
+    doer_details = UserSerializer(source='doer', read_only=True)
+    doer = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), default=serializers.CurrentUserDefault())
+
     order = serializers.PrimaryKeyRelatedField(
         queryset=Order.objects.all(), default=OrderUrlDefault())
 
@@ -21,8 +26,15 @@ class DoerRequestSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    owner = UserSerializer(default=serializers.CurrentUserDefault())
+    owner_details = serializers.StringRelatedField(
+        source='owner', read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), default=serializers.CurrentUserDefault())
+
+    doer_details = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), default=serializers.CurrentUserDefault())
     doer = serializers.StringRelatedField(read_only=True)
+
     requests = DoerRequestSerializer(many=True, read_only=True)
 
     class Meta:
