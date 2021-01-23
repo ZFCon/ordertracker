ebfcb2b90bc6727c4961722a3ff571a5097c2897
unknown <ziad32ahmed111@gmail.com>
2021-01-23 17:40:36 +0200

    update websocket route

--- order/routing.py
+++ order/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/orders/', consumers.OrderConsumer.as_asgi()),
+    path('ws/order/', consumers.OrderConsumer.as_asgi()),
 ]
